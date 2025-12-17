cf47be80e7cb51de7e4a28ca22849edb5e09d064
AhirraoShubham <spahirrao8@gmail.com>
2025-12-17 23:31:39 +0530

    01 Overview

---
+++ 01-Overview/myspringbootapp/src/main/java/com/shubham/springboot/myspringbootapp/rest/FunRestController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2025 Mr.Ahirrao - All Rights Reserved
+ * Unauthorized copying or redistribution of this file in source and binary forms
+ * via any medium is strictly prohibited.
+ */
+
+package com.shubham.springboot.myspringbootapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // Inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Expose new endpoint for "teamDetails"
+    @GetMapping("/teamdetails")
+    public String getTeamDetails() {
+        return "Coach: " + coachName + "Team name: " + teamName;
+    }
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+}
